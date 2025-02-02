5c8196e05ca6d75660e5857469f4b10376b81db4
devAseemSharma <er.sharmaaseem@gmail.com>
2025-02-02 13:49:27 +0530

    added theme

--- app/src/main/java/com/androidace/spendanalyser/MainActivity.kt
+++ app/src/main/java/com/androidace/spendanalyser/MainActivity.kt
@@ -5,20 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.androidace.spendanalyser.ui.theme.SpendAnalyserTheme
+import com.androidace.spendanalyser.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SpendAnalyserTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
                 }
